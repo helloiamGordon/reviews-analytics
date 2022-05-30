93576b28ed5d427bc177068e51a0831c78ed0be8
Gordon lo <gordon0427@hotmail.com>
2022-05-30 18:30:54 +0800

    calc avg length of reviews

--- read.py
+++ read.py
@@ -6,4 +6,10 @@ with open('reviews.txt', 'r') as f:
 		count += 1
 		if count % 10000 == 0 :
 			print(len(data))
-print(len(data))		
+print('檔案讀取完了,總共有', len(data), '筆資料')	
+
+sum_len = 0
+for d in data:
+	L=len(d)
+	sum_len = sum_len + L
+print('平均字數是',sum_len/len(data))	
